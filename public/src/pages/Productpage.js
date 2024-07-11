8c8788367b045caea2fef1ab99637ef6c6e3f9d6
RAJAT <135589300+SKULLDUDEE@users.noreply.github.com>
2024-07-11 13:51:01 +0530

    Add files via upload

---
+++ public/src/pages/Productpage.js
@@ -0,0 +1,17 @@
+
+import React from 'react';
+import ProductDetail from '../components/ProductDetail';
+import ProductImageCarousel from '../components/ProductImageCarousel';
+import ProductReviews from '../components/ProductReviews';
+
+function ProductPage() {
+  return (
+    <div className="product-page">
+      <ProductImageCarousel />
+      <ProductDetail />
+      <ProductReviews />
+    </div>
+  );
+}
+
+export default ProductPage;
